f36e1f94bbdecc4f1fae344ff265245c53c13703
dantezhu <dantezhu@qq.com>
2014-04-09 11:38:21 +0800

    版本

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="yunbk",
-    version='0.1.30',
+    version='0.1.31',
     zip_safe=False,
     platforms='any',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
--- yunbk/__init__.py
+++ yunbk/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.1.30'
+__version__ = '0.1.31'
 
 from .yunbk import YunBK
