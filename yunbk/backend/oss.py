46600c3dc7ac1dc806ed224ae5763fe16fe13191
dantezhu <dantezhu@qq.com>
2014-04-16 19:36:52 +0800

    增加oss支持

---
+++ yunbk/backend/oss.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+import os
+from .base import BaseBackend
+from oss.oss_api import OssAPI
+
+
+OSS_HOST = 'oss.aliyuncs.com'
+
+
+class OSSBackend(BaseBackend):
+    """
+    本地后端
+    """
+
+    bucket_name = None
+    oss = None
+
+    def __init__(self, access_id, secret_access_key, bucket_name, host=None):
+        super(OSSBackend, self).__init__()
+        self.bucket_name = bucket_name
+        self.oss = OssAPI(host or OSS_HOST, access_id, secret_access_key)
+
+    def upload(self, file_path, category):
+        """
+        上传
+        """
+
+        filename = os.path.basename(file_path)
+
+        # 尝试创建bucket
+        rsp = self.oss.create_bucket(self.bucket_name)
+
+        if rsp.status not in (200, 409):
+            # 说明没有创建成功
+            raise Exception('create_bucket fail: %s' % rsp)
+
+        rsp = self.oss.put_object_from_file(
+            self.bucket_name,
+            '/%s/%s' % (category, filename),
+            file_path,
+        )
+
+        if rsp.status != 200:
+            raise Exception('put_object_from_file fail: %s' % rsp)
