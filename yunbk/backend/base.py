266789e6b63a65b7aac174987d948960921c04e2
dantezhu <dantezhu@qq.com>
2014-05-16 12:34:41 +0800

    clean

--- yunbk/backend/base.py
+++ yunbk/backend/base.py
@@ -5,3 +5,6 @@ class BaseBackend(object):
 
     def upload(self, file_path, folder):
         raise NotImplementedError('not implemented')
+
+    def clean(self, category, keeps):
+        raise NotImplementedError('not implemented')
